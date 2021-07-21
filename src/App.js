d3811788c032f06afa02bcdba6747e1ef2a153b8
victorzottmann <zottmann.victor@gmail.com>
2021-07-21 22:50:16 +1000

    render csmiley face icon for all chicago locations

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { useRef, useEffect, useState } from "react"
-import mapboxgl from "!mapbox-gl" // eslint-disable-line import/no-webpack-loader-syntax
-import ReactMapGl from "react-map-gl"
+import React, { useState } from "react"
+import ReactMapGl, { Marker } from "react-map-gl"
 
-import "./App.css"
+import cityData from "./data/chicago-parks.json"
 
 function App() {
-  const mapContainer = useRef(null)
-  const map = useRef(null)
+  const mapboxToken = process.env.REACT_APP_MAPBOX_KEY
+  const mapStyle = "mapbox://styles/vzottmann/ckrdffg052fde18o24frcr99x"
 
   let coordinates = {
-    // latitude: -87.661557,
-    // longitude: 41.893748,
     chicago: {
       latitude: 41.893748,
       longitude: -87.661557,
@@ -21,58 +18,35 @@ function App() {
     },
   }
 
-  const [lat, setLat] = useState(coordinates.sydney.latitude)
-  const [lng, setLng] = useState(coordinates.sydney.longitude)
-  const [zoom, setZoom] = useState(12.2)
-
-  mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY
-
-  useEffect(() => {
-    if (map.current) return // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/vzottmann/ckrdffg052fde18o24frcr99x",
-      center: [lng, lat],
-      zoom: zoom,
-    })
-
-    if (!map.current) return // wait for map to initialize
-    map.current.on("move", () => {
-      setLng(map.current.getCenter().lng.toFixed(4))
-      setLat(map.current.getCenter().lat.toFixed(4))
-      setZoom(map.current.getZoom().toFixed(2))
-    })
+  const [viewport, setviewport] = useState({
+    latitude: coordinates.chicago.latitude,
+    longitude: coordinates.chicago.longitude,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
   })
 
-  // console.log(e)
-  // let features = map.queryRenderedFeatures(e.point, {
-  //   layers: ["paranormal-locations"],
-  // })
-  // if (!features.length) {
-  //   return
-  // }
-  // let feature = features[0]
-  // let popup = new mapboxgl.Popup({ offset: [0, -15] })
-  //   .setLngLat(feature.geometry.coordinates)
-  //   .setHTML(
-  //     "<h3>" +
-  //       feature.properties.title +
-  //       "</h3>" +
-  //       "<p>" +
-  //       feature.properties.description +
-  //       "</p>"
-  //   )
-  //   .addTo(map)
-
-  console.log(map.current.on())
-
   return (
-    <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
-    </div>
+    <>
+      <ReactMapGl
+        {...viewport}
+        mapboxApiAccessToken={mapboxToken}
+        onViewportChange={(viewport) => {
+          setviewport(viewport)
+        }}
+        mapStyle={mapStyle}
+      >
+        {cityData.features.map((feature) => (
+          <Marker
+            key={feature.properties.title}
+            latitude={feature.geometry.coordinates[1]}
+            longitude={feature.geometry.coordinates[0]}
+          >
+            <div>🙂</div>
+          </Marker>
+        ))}
+      </ReactMapGl>
+    </>
   )
 }
 
